0246c977eb01379bb8773bba44574a4136d61aff
ekrem0691 <06hilalekrem06@gmail.com>
2022-10-14 21:17:39 +0300

    added example

---
+++ src/components/styles/Button.styled.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+    background-color: ${({bg})=> bg || "#fff" };
+    color: ${({color})=> color || "#fff" };
+`;
+
+export default Button;
